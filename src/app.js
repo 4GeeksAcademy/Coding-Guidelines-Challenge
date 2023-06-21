f85790338baa0be11393adc554cb86ad43afe9d7
Sol-max <59577486+Sol-max@users.noreply.github.com>
2023-06-21 18:12:10 +0000

    reviewed code

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import "bootstrap"; // Importing the Bootstrap library
+import "./style.css"; // Importing a CSS file
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //main function that generates random outputs
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  // Generate random values from the arrays
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  // Create an excuse using the random values
+  let excuse =
+    randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen;
+
+  // Setthe generated excuse as the content of an element with the id "excuse"
+  document.querySelector("#excuse").innerHTML = excuse;
 };
